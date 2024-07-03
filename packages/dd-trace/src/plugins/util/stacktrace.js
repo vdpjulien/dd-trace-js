710990f3277b0232d875a0036c3068bdbf8ff7e9
Thomas Watson <w@tson.dk>
2024-07-03 13:27:34 +0200

    Improve algo

--- packages/dd-trace/src/plugins/util/stacktrace.js
+++ packages/dd-trace/src/plugins/util/stacktrace.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { relative, sep } = require('path')
+const { relative, sep, isAbsolute } = require('path')
 
 const cwd = process.cwd()
 
@@ -34,17 +34,40 @@ function getCallSites (constructorOpt) {
 function getUserLandCallsites (constructorOpt = getUserLandCallsites, returnTopUserLandFrameOnly = false) {
   const callsites = getCallSites(constructorOpt)
   for (let i = 0; i < callsites.length; i++) {
-    const fullPath = callsites[i].getFileName()
+    const callsite = callsites[i]
 
-    if (fullPath === null) {
+    if (callsite.isNative()) {
       continue
     }
-    // *.mjs paths start with the "file://" protocol because ESM supports https imports
-    const containsFileProtocol = fullPath.startsWith('file://')
-    if (fullPath.startsWith(cwd, containsFileProtocol ? 7 : 0) === false) {
+
+    const filename = callsite.getFileName()
+
+    // If the callsite is native, there will be no associated filename. However, there might be other instances where
+    // this can happen, so to be sure, we add this additional check
+    if (filename === null) {
       continue
     }
-    const relativePath = relative(fullPath, containsFileProtocol ? fullPath.substring(7) : fullPath)
+
+    // ESM module paths start with the "file://" protocol (because ESM supports https imports)
+    // TODO: Node.js also supports `data:` and `node:` imports, should we do something specific for `data:`?
+    const containsFileProtocol = filename.startsWith('file:')
+
+    // TODO: I'm not sure how stable this check is. Alternatively, we could consider reversing it if we can get
+    // a comprehensive list of all non-file-based values, eg:
+    //
+    //     filename === '<anonymous>' || filename.startsWith('node:')
+    if (containsFileProtocol === false && isAbsolute(filename) === false) {
+      continue
+    }
+
+    // TODO: Technically, the algorithm below could be simplified to not use the relative path, but be simply:
+    //
+    //     if (fullPath.includes(sep + 'node_modules' + sep)) continue
+    //
+    // However, the tests in `packages/dd-trace/test/plugins/util/stacktrace.spec.js` will fail on my machine
+    // because I have the source code in a parent folder called `node_modules`. So the code below thinks that
+    // it's not in user-land
+    const relativePath = relative(cwd, containsFileProtocol ? filename.substring(7) : filename)
     if (relativePath.startsWith('node_modules' + sep) || relativePath.includes(sep + 'node_modules' + sep)) {
       continue
     }
