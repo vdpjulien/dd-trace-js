76cce7282449d1ef0aba8334f0b9464d588446c2
Thomas Watson <w@tson.dk>
2024-07-03 14:58:20 +0200

    Improve test coverage

--- packages/dd-trace/test/plugins/util/web.spec.js
+++ packages/dd-trace/test/plugins/util/web.spec.js
@@ -139,6 +139,22 @@ describe('plugins/util/web', () => {
     })
   })
 
+  describe('setSpanTags', () => {
+    it('should add expected tags', () => {
+      web.instrument(tracer, config, req, res, 'test.request', span => {
+        web.setSpanTags(req, { foo: 'bar' })
+
+        const tags = span.context()._tags
+
+        res.end()
+
+        expect(tags).to.include({
+          foo: 'bar'
+        })
+      })
+    })
+  })
+
   describe('instrument', () => {
     describe('on request start', () => {
       it('should set the parent from the request headers', () => {
